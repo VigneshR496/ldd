b619c4a21b270eb6240d031971fb57e46a76afc3
vignesh-ravikumar <vignesh.ravikumar@rently.com>
2024-07-10 17:39:06 +0530

    creating device file

---
+++ L4-device_file/device_file.c
@@ -0,0 +1,69 @@
+/***************************************************************************/ /**
+                                                                               *  \file       driver.c
+                                                                               *
+                                                                               *  \details    Simple linux driver (Automatically creating a Device File)
+                                                                               *
+                                                                               *  \author     Vignesh R
+                                                                               *
+                                                                               * *******************************************************************************/
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/err.h>
+#include <linux/device.h>
+
+dev_t dev = 0;
+static struct class *dev_class;
+
+/*
+** Module Init function
+*/
+static int __init hello_world_init(void) {
+  if(alloc_chrdev_region(&dev, 0, 1, "My_Dev") < 0){
+    pr_err("Cannot allocate major number for device\n");
+    return -1;
+  }
+  printk(KERN_INFO "Major = %d, Minor = %d\n", MAJOR(dev), MINOR(dev));
+
+  dev_class = class_create(THIS_MODULE, "my_class");
+  if(IS_ERR(dev_class)){
+    pr_err("Cannot create the struct clzss for device\n");
+    goto r_class;
+  }
+
+  if(IS_ERR(device_create(dev_class, NULL, dev, NULL, "my_device"))){
+    pr_err("Cannot create the Device");
+    goto r_device;
+  }
+
+  printk(KERN_INFO "Kernel Module Inserted Successfully...\n");
+  return 0;
+
+  r_device:
+  class_destroy(dev_class);
+
+  r_class:
+  unregister_chrdev_region(dev, 1);
+
+  return -1;
+}
+
+/*
+** Module Exit function
+*/
+static void __exit hello_world_exit(void) {
+  device_destroy(dev_class, dev);
+  class_destroy(dev_class);
+  unregister_chrdev_region(dev, 1);
+  printk(KERN_INFO "Kernel Module Removed Successfully...\n");
+}
+
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Vignesh R");
+MODULE_DESCRIPTION("A simple hello world driver");
+MODULE_VERSION("2:1.0");
